8ed61c1db7c020787fc2892b40c4faef041d4ac1
Prince JAT <choudharyprince140@gmail.com>
2023-06-08 20:07:53 +0530

    Add files via upload

--- first.dart
+++ first.dart
@@ -1,5 +1,22 @@
 void main(){
+    int a=10;
+    int b=20;
+
     String name="prince";
     String sirname = "JAT";
-    print(name+" "+sirname);
+    print(name+"\n"+sirname);
+    int add (a,b){
+        return a+b;
+};
+     loop(a){
+   
+        for( var i =a ; i >= 1; i-- ) { 
+            print(i);
+   } 
+    };
+print(loop(5));
+print(add(a,b));
+    
 }
+
+
